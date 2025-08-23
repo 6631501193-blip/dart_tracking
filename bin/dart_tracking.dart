c6594e6c9561e5bf6b0d1c92056483d21d46506e
yoon <6631501193@lamduan.mfu.ac.th>
2025-08-23 14:37:04 +0700

    initial commit

---
+++ bin/dart_tracking.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tracking/dart_tracking.dart' as dart_tracking;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tracking.calculate()}!');
+}
---
+++ test/dart_tracking_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tracking/dart_tracking.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
